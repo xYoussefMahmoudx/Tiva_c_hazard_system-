eabf591fce467a974942537bae6b57d57f874221
xYoussefMahmoudx <youssef.mahmoud.eng@gmail.com>
2023-12-28 18:02:18 +0200

    bluetooth worked

--- main.c
+++ main.c
@@ -37,12 +37,13 @@ int main()
 	SYSCTL_RCGCGPIO_R |= 0x20;   /* enable clock to GPIOF */
         GPIO_PORTF_DIR_R |= 0x0E;         //set PF1, PF2 and PF3 as digital output pin
         GPIO_PORTF_DEN_R |= 0x0E;         // CON PF1, PF2 and PF3 as digital GPIO pins
-  	Delay(10); 
+  	
 	
 	while(1)
 	{
+         
 		char c = Bluetooth_Read();          /* get a character from UART5 */
-		
+		 printf("%c\n",c);
 /* Check the received character and take action to control onboard LEDs of TM4C123 */
 /* Send status string to Andriod app after turning on/off LEDs */
 
@@ -62,7 +63,7 @@ int main()
 			
 			Bluetooth_Write_String("BLUE LED OFF\n");
 		}
-		if( c=='E'){
+		else if( c=='E'){
 			
 			Bluetooth_Write_String("GREEN LED ON\n");
 		}
@@ -72,7 +73,9 @@ int main()
 		}	
 	
 
-  return 0;
-}
 
+
+
+}
+  return 0;
 }
